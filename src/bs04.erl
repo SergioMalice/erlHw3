059491d9c62821e654e33ea87f329d905f050763
SergeyB <SergioAsterix@gmail.com>
2019-07-05 17:10:11 +0300

    erlang.mk build, оптимизация 4й, проплисты

--- src/bs04.erl
+++ src/bs04.erl
@@ -15,15 +15,15 @@
 % успел сделать парсер в проплисты. Тест для копирования ниже. Комментарии объяснят логику кода
 % bs04:decode(<<"{'squadName': 'Super hero squad','homeTown': 'Metro City','formed': 2016,'secretBase': 'Super tower','active': true,'members': [{'name': 'Molecule Man','age': 29,'secretIdentity': 'Dan Jukes','powers': ['Radiation resistance','Turning tiny','Radiation blast']},{'name': 'Madame Uppercut','age': 39,'secretIdentity': 'Jane Wilson','powers': ['Million tonne punch','Damage resistance','Superhuman reflexes']},{'name': 'Eternal Flame','age': 1000000,'secretIdentity': 'Unknown','powers': ['Immortality','Heat Immunity','Inferno','Teleportation','Interdimensional travel']}] } ">>, proplist).
 
-decode(<<"{", Rest/binary>>, Method) when Method == proplist -> prop(Rest, key, 1, <<>>, [], []).
+decode(<<"{", Rest/binary>>, Method) when Method == proplist -> prop(clear_input(Rest, no, <<>>), key, 1, <<>>, [], []).
 % пропускаем начало входа, чтобы не было совпадения с { началом обработки "вложенного json"
 %%decode(Json, Method) -> map(Json, map)
 
 prop(<<C, Rest/binary>>, Flag, InnerCnt, Bin, List, Res) when (C == 58) or (C == 44) -> %двоеточие и , смена флага
-  case Flag of
-    key -> NewFlag = value;
-    value -> NewFlag = key;
-    inner -> NewFlag = inner
+  NewFlag = case Flag of
+    key -> value;
+    value -> key;
+    inner -> inner
   end,
   prop(Rest, NewFlag, InnerCnt, Bin, List, Res);
 prop(<<"'", Rest/binary>>, key, InnerCnt, _Bin, List, Res) -> %кавычка с флагом ключа
@@ -45,11 +45,11 @@ prop(<<"{", Rest/binary>>, Flag, Cnt, Bin, List, Res) -> % { - начинаем 
   case (Cnt == 1) of
     true ->
       case Flag of
-        inner -> prop(skip(Rest, Count), value, Cnt, Bin, [InnerRes|List], Res);
+        inner -> prop(skip(Rest, Count+1), value, Cnt, Bin, [InnerRes|List], Res);
         _ ->
           case Bin == <<>> of
-            false -> prop(skip(Rest, Count), value, Cnt, Bin, [InnerRes|List], Res);
-            _ -> prop(skip(Rest, Count), value, Cnt, Bin, List, [InnerRes|Res])
+            false -> prop(skip(Rest, Count+1), value, Cnt, Bin, [InnerRes|List], Res);
+            _ -> prop(skip(Rest, Count+1), value, Cnt, Bin, List, [InnerRes|Res])
           end
       end;
     _ -> {Bin, InnerRes}
@@ -60,34 +60,35 @@ prop(<<"[", Rest/binary>>, _Flag, InnerCnt, Bin, _List, Res) -> % [ - начин
   prop(Rest, inner, InnerCnt, Bin, [], Res); % начали запись значения в кортеж со значением - списком
 prop(<<"]", Rest/binary>>, _Flag, InnerCnt, Bin, List, Res) -> % [ - заканчиваем запись вложенного списка
   prop(Rest, value, InnerCnt, <<>>, <<>>, [{Bin, reverse(List)}|Res]);
-prop(<<Skip, Rest/binary>>, Flag, InnerCnt, Bin, List, Res) when
-  (Skip == 32) or % пробел пропускаем
-    (Skip == 13) -> % перевод строки пропускаем
-  prop(Rest, Flag, InnerCnt, Bin, List, Res);
 prop(<<C/utf8, Rest/binary>>, value, InnerCnt, Bin, List, Res) -> % символ с флагом значения - true, false или число
   Value = read_key(<<C/utf8, Rest/binary>>, <<>>, atom),
   ValueSize = byte_size(Value)-1,
   <<_:ValueSize/binary, NewRest/binary>> = Rest,
   prop(NewRest, value, InnerCnt, <<>>, List, [{Bin, parse_value(Value)}|Res]); %записываем кортеж в результат
-prop(<<_/utf8, Rest/binary>>, Flag, InnerCnt, Bin, List, Res) -> prop(Rest, Flag, InnerCnt, Bin, List, Res);
 prop(<<>>, _, _, _, _, Res) -> reverse(Res).
+
+% убираем все пробелы, кроме тех, что в ключах/значениях и переводы строк на входе
+clear_input(<<" ", Rest/binary>>, no, _Res) -> clear_input(Rest, no, _Res);
+clear_input(<<"'", Rest/binary>>, NeedSpace, Res) ->
+  clear_input(Rest, case NeedSpace of yes -> no; _ -> yes end, <<Res/binary, "'">>);
+clear_input(<<" ", Rest/binary>>, yes, Res) -> clear_input(Rest, yes, <<Res/binary, " ">>);
+clear_input(<<13, Rest/binary>>, _NeedSpace, _Res) -> clear_input(Rest, _NeedSpace, _Res);
+clear_input(<<C/utf8, Rest/binary>>, _NeedSpace, Res) -> clear_input(Rest, _NeedSpace, <<Res/binary, C>>);
+clear_input(<<>>, _NeedSpace, Res) -> Res.
 
 read_inner(<<"}">>, Res, Cnt, _) -> {Res, Cnt};
 read_inner(<<"}", _/binary>>, Res, Cnt, 0) -> {Res, Cnt};
 read_inner(<<"}", Rest/binary>>, Res, Cnt, InnerCnt) -> read_inner(Rest, <<Res/binary, "}">>, Cnt+1, InnerCnt-1);
-read_inner(<<C, Rest/binary>>, Res, Cnt, InnerCnt) when (C == 32) or (C == 13) -> read_inner(Rest, Res, Cnt, InnerCnt);
 read_inner(<<"{", Rest/binary>>, Res, Cnt, InnerCnt) -> read_inner(Rest, <<Res/binary, "{">>, Cnt+1, InnerCnt+1);
 read_inner(<<C/utf8, Rest/binary>>, Res, Cnt, InnerCnt) -> read_inner(Rest, <<Res/binary, C/utf8>>, Cnt+1, InnerCnt);
 read_inner(<<>>, Res, Cnt, _) -> {Res, Cnt}.
 
-skip(Rest, -1) -> Rest;
-skip(<<C, Rest/binary>>, Cnt) when (C == 32) or (C == 13) -> skip(Rest, Cnt);
-skip(<<_/utf8, Rest/binary>>, Cnt) -> skip(Rest, Cnt-1).
+skip(String, Cnt) -> <<_:Cnt/binary, Rest/binary>> = String, Rest.
 
 read_key(<<"'", _/binary>>, Key, string) -> Key; % поиск строки. Конец - кавычка
 read_key(<<",", _/binary>>, Key, atom) -> Key; % поиск true false или числа. Конец - запятая или конец json
-read_key(<<"}">>, Key, _Flag) -> Key; % поиск true false или числа. Конец - запятая или конец json
 read_key(<<"}", _/binary>>, Key, _Flag) -> Key; % поиск true false или числа. Конец - запятая или конец json
+read_key(<<"}">>, Key, _Flag) -> Key; % поиск true false или числа. Конец - запятая или конец json
 read_key(<<C/utf8, Rest/binary>>, Key, _Flag) -> read_key(Rest, <<Key/binary, C/utf8>>, _Flag);
 read_key(<<>>, Key, atom) -> Key.
 
